893d1277d0ffc58e5686d687adcd06c491f70f00
Default <Default@LaptopHUAWEI-Pabular>
2023-02-24 15:15:15 -0800

    first commit

---
+++ MO-IT101MotorPH/src/Task7EmployeeDetails.java
@@ -0,0 +1,368 @@
+
+public class Task7EmployeeDetails {
+
+    public void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        int Enumber;
+    	String Lname,FName,BDay,Add;
+        int Phone;
+    
+    
+        Enumber = 10001;
+        Lname ="Crisostomo";
+        FName ="Jose";
+        BDay = "February 14, 1988";
+        Add ="17/85 Stracke Via Suite 042, Poblacion, Las Piñas 4783 Dinagat Islands ";
+        Phone = 918621603;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+   
+        Enumber = 10002;
+        Lname ="Mata";
+        FName ="Christian";
+        BDay = "October 21, 1987";
+        Add ="90 O'Keefe Spur Apt. 379, Catigbian 2772 Sulu ";
+        Phone = 783776744;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+    
+        Enumber = 10003;
+        Lname ="San Jose";
+        FName ="Brad";
+        BDay = "March 15, 1996";
+        Add ="99 Strosin Hills, Poblacion, Bislig 5340 Tawi-Tawi";
+        Phone = 797009261;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10004;
+        Lname ="Salcedo";
+        FName ="Anthony";
+        BDay = "September 14, 1993";
+        Add ="93/54 Shanahan Alley Apt. 183, Santo Tomas 1572 Masbate";
+        Phone = 070766300;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10005;
+        Lname ="Romualdez";
+        FName ="Alice";
+        BDay = "May 14, 1992";
+        Add ="12A/33 Upton Isle Apt. 420, Roxas City 1814 Surigao del Norte";
+        Phone = 983606799;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10006;
+        Lname ="Atienza";
+        FName ="Rosie";
+        BDay = "September 24, 1948";
+        Add ="90A Dibbert Terrace Apt. 190, San Lorenzo 6056 Davao del Norte";
+        Phone = 266036427;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10007;
+        Lname ="Farala";
+        FName ="Martha";
+        BDay = "January 11, 1942";
+        Add ="42/25 Sawayn Stream, Ubay 1208 Zamboanga del Norte";
+        Phone = 329034366;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10008;
+        Lname ="Martinez";
+        FName ="Leila";
+        BDay = "July 11, 1970";
+        Add ="37/46 Kulas Roads, Maragondon 0962 Quirino  ";
+        Phone = 877110749;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10009;
+        Lname ="San Jose";
+        FName ="Allison";
+        BDay = "June 24, 1986";
+        Add ="08 Grant Drive Suite 406, Poblacion, Iloilo City 9186 La Union";
+        Phone = 179075129;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10010;
+        Lname ="Rosario";
+        FName ="Cydney";
+        BDay = "October 6, 1996";
+        Add ="93A/21 Berge Points, Tapaz 2180 Quezon";
+        Phone = 868819912;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10011;
+        Lname ="Lopez";
+        FName ="Josie";
+        BDay = "January 14, 1937";
+        Add ="49 Springs Apt. 266, Poblacion, Taguig 3200 Occidental Mindoro";
+        Phone = 478355427;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10012;
+        Lname ="De Leon";
+        FName ="Selena";
+        BDay = "February 20, 1945";
+        Add ="89A Armstrong Trace, Compostela 7874 Maguindanao";
+        Phone = 975432139;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10013;
+        Lname ="Romualdez";
+        FName ="Fredrick";
+        BDay = "March 10, 1985";
+        Add ="22A/52 Lubowitz Meadows, Pililla 4895 Zambales";
+        Phone = 23079009;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10014;
+        Lname ="Bautista";
+        FName ="Mark";
+        BDay = "February 12, 1961";
+        Add ="65 Murphy Center Suite 094, Poblacion, Palayan 5636 Quirino";
+        Phone = 683725348;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10015;
+        Lname ="Lazaro";
+        FName ="Darlene";
+        BDay = "November 25, 1985";
+        Add ="47A/94 Larkin Plaza Apt. 179, Poblacion, Caloocan 2751 Quirino";
+        Phone = 740721558;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10016;
+        Lname ="Delos Santos";
+        FName ="Kolby ";
+        BDay = "February 26, 1980";
+        Add ="06A Gulgowski Extensions, Bongabon 6085 Zamboanga del Sur";
+        Phone = 739443033;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+    
+        Enumber = 10017;
+        Lname ="Santos";
+        FName ="Vella";
+        BDay = "December 31, 1983";
+        Add ="99A Padberg Spring, Poblacion, Mabalacat 3959 Lanao del Sur";
+        Phone = 955879269;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10018;
+        Lname ="Del Rosario";
+        FName ="Tomas";
+        BDay = "December 18, 1978";
+        Add ="80A/48 Ledner Ridges, Poblacion, Kabankalan 8870 Marinduque";
+        Phone = 882550989;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10019;
+        Lname ="Tolentino";
+        FName ="Jacklyn";
+        BDay = "May 19, 1944";
+        Add ="96/48 Watsica Flats Suite 734, Poblacion, Malolos 1844 Ifugao";
+        Phone = 675757366;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10020;
+        Lname ="Gutierrez";
+        FName ="Percival";
+        BDay = "December 18, 1970";
+        Add ="58A Wilderman Walks, Poblacion, Digos 5822 Davao del Sur";
+        Phone = 512899876;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10021;
+        Lname ="Manalaysay";
+        FName ="Garfield";
+        BDay = "August 28, 1966";
+        Add ="60 Goyette Valley Suite 219, Poblacion, Tabuk 3159 Lanao del Sur";
+        Phone = 948628136;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10022;
+        Lname ="Villegas";
+        FName ="Lizeth";
+        BDay = "December 12, 1981";
+        Add ="66/77 Mann Views, Luisiana 1263 Dinagat Islands";
+        Phone = 332372215;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10023;
+        Lname ="Ramos";
+        FName ="Carol";
+        BDay = "August 20, 1978";
+        Add ="72/70 Stamm Spurs, Bustos 4550 Iloilo";
+        Phone = 250700389;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10024;
+        Lname ="Maceda";
+        FName ="Emelia";
+        BDay = "April 14, 1963";
+        Add ="50A/83 Bahringer Oval Suite 145, Kiamba 7688 Nueva Ecija";
+        Phone = 973358041;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+        Enumber = 10025;
+        Lname ="Aguilar";
+        FName ="Delia";
+        BDay = "January 27, 1939";
+        Add ="95 Cremin Junction, Surallah 2809 Cotabato";
+        Phone = 529705439;
+        System.out.println("****************");
+        System.out.println("*Employee Number:" + Enumber);
+        System.out.println("*Last Name:" + Lname);
+        System.out.println("*First Name:" + FName);
+        System.out.println("*Birthday:" + BDay);
+        System.out.println("*Address:" + Add);
+        System.out.println("*Phone Number:" + Phone);
+        
+       
+        
+        
+    }
+
+	
+}
